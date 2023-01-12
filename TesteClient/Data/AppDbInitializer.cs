907048f6f3bb45af5e3d7e32250d61c1dcb4abb0
TarikAliouchouche <johndoe@example.com>
2023-01-12 14:04:59 +0100

    Ajouts de Modeles Produits

--- TesteClient/Data/AppDbInitializer.cs
+++ TesteClient/Data/AppDbInitializer.cs
@@ -1,10 +1,12 @@
 ﻿using Aspose.Cells;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 using TesteClient.Data;
 using TesteClient.Models;
+using TesteClient.Models.Products;
 
 public class AppDbInitializer
 {
@@ -16,64 +18,65 @@ public class AppDbInitializer
 
             context.Database.EnsureCreated();
             //Table Categrorie
-            if (!context.Categories.Any())
+            //if (!context.Categories.Any())
+            //{
+            //    context.Categories.AddRange(new List<Category>()
+            //    {
+
+            //        new Category()
+            //        {
+
+            //            CategoryName = "Reflex",
+            //            Description = "Appareils Type 2",
+
+            //        },
+
+            //        new Category()
+            //        {
+
+            //            CategoryName = "Reflex",
+            //            Description = "Appareils Type 2",
+
+            //        }
+            //    });
+            //    context.SaveChanges();
+
+
+
+            if (!context.Products.Any())
             {
-                context.Categories.AddRange(new List<Category>()
+                context.Products.AddRange(new List<Product>()
                 {
 
-                    new Category()
+                    new Product()
                     {
-
-                        CategoryName = "Reflex",
-                        Description = "Appareils Type 2",
+                        name = "Canon D80",
+                        productTechnoId =1,
+                        productBrandId=1,
+                        price=1200,
+                        productDetailsId=1
 
                     },
 
-                    new Category()
+                    new Product()
                     {
-
-                        CategoryName = "Reflex",
-                        Description = "Appareils Type 2",
+                        name = "Canon D80",
+                        productTechnoId = 1,
+                        productBrandId=1,
+                        price=1400,
+                        productDetailsId=1
 
                     }
                 });
                 context.SaveChanges();
+            }
 
+            int ICivility = 1;
 
+            if (!context.ApplicationUser.Any())
 
-                if (!context.Products.Any())
-                {
-                    context.Products.AddRange(new List<Product>()
-                {
-
-                    new Product()
-                    {
-                        ProductName = "Canon D750",
-                        Description = "Appareil photo Reflex",
-                        ImagePath = "EMG\\CanonD750.JPG",
-                        UnitPrice = 450,
-                        CategoryID = 1
-                    },
-
-                    new Product()
-                    {
-                        ProductName = "Canon D80",
-                        Description = "Appareil photo Reflex",
-                        ImagePath = "EMG\\Canon80.JPG",
-                        UnitPrice = 890,
-                        CategoryID = 1,
-
-                    }
-                });
-                    context.SaveChanges();
-                }
-
-                int ICivility=1;
-                
-                if (!context.ApplicationUser.Any())
-                    
-                {
-                    context.ApplicationUser.AddRange(new List<ApplicationUser>()
+            {
+                context.ApplicationUser.AddRange(new List<ApplicationUser>()
                             {
 
                         new ApplicationUser()
@@ -104,64 +107,64 @@ public class AppDbInitializer
                                     BirthDate = new DateTime(10/10/1977),
                                 }
                                 });
-                    context.SaveChanges();
-                }
+                context.SaveChanges();
+            }
 
 
 
 
-                ////-------- Application User
+            ////-------- Application User
 
-                //Workbook wb = new Workbook("D:\\Ecommerce.xlsx");
-                //WorksheetCollection collection = wb.Worksheets;
-                ////Application User
-
-
+            //Workbook wb = new Workbook("D:\\Ecommerce.xlsx");
+            //WorksheetCollection collection = wb.Worksheets;
+            ////Application User
 
 
-                //for (int worksheetIndex = 0; worksheetIndex < collection.Count; worksheetIndex++)
-                //{
-
-                //    Worksheet worksheet = collection[worksheetIndex];
 
 
-                //    int rows = worksheet.Cells.MaxDataRow;
-                //    int cols = worksheet.Cells.MaxDataColumn;
-                //    //string Ville;
+            //for (int worksheetIndex = 0; worksheetIndex < collection.Count; worksheetIndex++)
+            //{
+
+            //    Worksheet worksheet = collection[worksheetIndex];
 
 
-                //    worksheetIndex = 0;
+            //    int rows = worksheet.Cells.MaxDataRow;
+            //    int cols = worksheet.Cells.MaxDataColumn;
+            //    //string Ville;
 
-                //    for (int i = 0; i < rows; i++)
-                //    {
-                //        if (!context.ApplicationUser.Any())
-                //        {
-                //            context.ApplicationUser.AddRange(new List<ApplicationUser>()
-                //            {
-                //                new ApplicationUser()
-                //                {
-                //                    UserName = Convert.ToString(worksheet.Cells[i, 0].Value),
-                //                    PasswordHash = Convert.ToString(worksheet.Cells[i, 1].Value),
-                //                    Email = Convert.ToString(worksheet.Cells[i, 3].Value),
-                //                    EmailConfirmed = true,
-                //                    PhoneNumber = Convert.ToString(worksheet.Cells[i, 2].Value),
-                //                    PhoneNumberConfirmed = true,
-                //                    Civility = (ApplicationUser.eCivility)worksheet.Cells[i, 4].Value,
-                //                    LastName = Convert.ToString(worksheet.Cells[i, 5].Value),
-                //                    FirstName = Convert.ToString(worksheet.Cells[i, 6].Value),
-                //                    BirthDate = Convert.ToDateTime(worksheet.Cells[i, 7].Value),
-                //                }
-                //                 });
-                //            context.SaveChanges();
 
-                //        }
+            //    worksheetIndex = 0;
 
-                //    }
+            //    for (int i = 0; i < rows; i++)
+            //    {
+            //        if (!context.ApplicationUser.Any())
+            //        {
+            //            context.ApplicationUser.AddRange(new List<ApplicationUser>()
+            //            {
+            //                new ApplicationUser()
+            //                {
+            //                    UserName = Convert.ToString(worksheet.Cells[i, 0].Value),
+            //                    PasswordHash = Convert.ToString(worksheet.Cells[i, 1].Value),
+            //                    Email = Convert.ToString(worksheet.Cells[i, 3].Value),
+            //                    EmailConfirmed = true,
+            //                    PhoneNumber = Convert.ToString(worksheet.Cells[i, 2].Value),
+            //                    PhoneNumberConfirmed = true,
+            //                    Civility = (ApplicationUser.eCivility)worksheet.Cells[i, 4].Value,
+            //                    LastName = Convert.ToString(worksheet.Cells[i, 5].Value),
+            //                    FirstName = Convert.ToString(worksheet.Cells[i, 6].Value),
+            //                    BirthDate = Convert.ToDateTime(worksheet.Cells[i, 7].Value),
+            //                }
+            //                 });
+            //            context.SaveChanges();
 
-                }
+            //        }
 
-            
+            //    }
+
         }
+
+
     }
+}
 }
 
